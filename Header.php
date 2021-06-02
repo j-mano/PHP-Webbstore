405673a58d6c7a42695351d8e477d5bbdf5971cd
joachim <joachim@johnsons.nu>
2021-06-02 21:10:03 +0200

    Inital build
    html and css set upp

---
+++ Header.php
@@ -0,0 +1,12 @@
+<header>
+	<div class="Header">
+		<img class="logo" src="\Images\CompanyLogo.png" alt="Error-404.jpg" width="500" height="600">
+
+		<div class="vertical-menu">
+			<a href="index.PHP"		>Home</a>
+			<a href="Products.PHP"	>Products</a>
+			<a href="About.PHP"		>About the company</a>
+			<a href="Contacts.PHP"	>Contacts</a>
+		</div>
+	</div>
+</header>
---
+++ footer.php
@@ -0,0 +1,4 @@
+<footer class="footer">
+  <p>Writen by: Joachim Johnson</p>
+  <p>This webbsite is writen to Learn Php</p>
+</footer>
